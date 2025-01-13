86548bcdf7075ad99fd82d1eb18d49e364729751
Joshua Penuela <joshuapenuela12@gmail.com>
2025-01-13 18:25:56 +0800

    1st

---
+++ footer.php
@@ -0,0 +1,43 @@
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="home.php">Home</a>
+            <a href="about.php">About</a>
+            <a href="contact.php">Contact</a>
+            <a href="shop.php">Shop</a>
+        </div>
+
+        <div class="box">
+            <h3>extra links</h3>
+            <a href="login.php">Login</a>
+            <a href="register.php">Register</a>
+            <a href="orders.php">My orders</a>
+            <a href="cart.php">My cart</a>
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <p> <i class="fas fa-phone"></i> 0999 327 9609 (Smart) </p>
+            <p> <i class="fas fa-phone"></i> 0927 420 6996 (Globe) </p>
+            <p> <i class="fas fa-envelope"></i> inquire@sheshdrip.com </p>
+            <p> <i class="fas fa-map-marker-alt"></i> Dasmariñas City, Cavite, Philippines </p>
+        </div>
+
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="#"><i class="fab fa-facebook-f"></i>Sheesh Drip</a>
+            <a href="#"><i class="fab fa-x"></i>@SheeshDrip</a>
+            <a href="#"><i class="fab fa-instagram"></i>@SheeshDrip</a>
+            <a href="#"><i class="fab fa-youtube"></i>Sheesh Drip</a>
+        </div>
+
+    </div>
+
+    <div class="credit">&copy; Copyright <?php echo date('Y'); ?> <span><br>
+    BSIT 4-1 • ITEC 111 • FINALS
+    </span> </div>
+
+</section>
