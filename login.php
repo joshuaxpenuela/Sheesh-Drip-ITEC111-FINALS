6d96126a71b38ccc7d6d081f5efb1891ca04e889
Joshua Penuela <joshuapenuela12@gmail.com>
2025-01-16 02:16:04 +0800

    2nd part four

--- login.php
+++ login.php
@@ -4,22 +4,17 @@
 
 session_start();
 
-// Function to encrypt password using Caesar cipher with key 13
+// Function to encrypt password (same as register.php)
 function caesarCipher($str, $shift = 13) {
     $encrypted = '';
-    // Loop through each character in the string
     for ($i = 0; $i < strlen($str); $i++) {
         $char = $str[$i];
-        
-        // Handle uppercase letters
         if (ctype_upper($char)) {
             $encrypted .= chr((ord($char) + $shift - 65) % 26 + 65);
         }
-        // Handle lowercase letters
         elseif (ctype_lower($char)) {
             $encrypted .= chr((ord($char) + $shift - 97) % 26 + 97);
         }
-        // Keep numbers and special characters as is
         else {
             $encrypted .= $char;
         }
@@ -27,41 +22,62 @@ function caesarCipher($str, $shift = 13) {
     return $encrypted;
 }
 
+// Function to decrypt Caesar cipher
+// Uses negative shift value to reverse the encryption
+function caesarDecipher($str, $shift = 13) {
+    // To decrypt, we shift in the opposite direction
+    // For shift of 13, we can use -13 or 13 (since 26-13=13)
+    // For other shift values, use 26 - shift
+    $decrypted = '';
+    for ($i = 0; $i < strlen($str); $i++) {
+        $char = $str[$i];
+        if (ctype_upper($char)) {
+            // Add 26 before modulo to handle negative shifts
+            $decrypted .= chr((ord($char) - $shift - 65 + 26) % 26 + 65);
+        }
+        elseif (ctype_lower($char)) {
+            $decrypted .= chr((ord($char) - $shift - 97 + 26) % 26 + 97);
+        }
+        else {
+            $decrypted .= $char;
+        }
+    }
+    return $decrypted;
+}
+
 if(isset($_POST['submit'])){
 
    $filter_email = filter_var($_POST['email'], FILTER_SANITIZE_STRING);
    $email = mysqli_real_escape_string($conn, $filter_email);
    $filter_pass = filter_var($_POST['pass'], FILTER_SANITIZE_STRING);
-   $pass = mysqli_real_escape_string($conn, caesarCipher($filter_pass));
-
-   $select_users = mysqli_query($conn, "SELECT * FROM `users` WHERE email = '$email' AND password = '$pass'") or die('query failed');
-
+   
+   // Get encrypted password from database first
+   $select_users = mysqli_query($conn, "SELECT * FROM `users` WHERE email = '$email'") or die('query failed');
+   
    if(mysqli_num_rows($select_users) > 0){
-      
       $row = mysqli_fetch_assoc($select_users);
-
-      if($row['user_type'] == 'admin'){
-
-         $_SESSION['admin_name'] = $row['name'];
-         $_SESSION['admin_email'] = $row['email'];
-         $_SESSION['admin_id'] = $row['id'];
-         header('location:admin_page.php');
-
-      }elseif($row['user_type'] == 'user'){
-
-         $_SESSION['user_name'] = $row['name'];
-         $_SESSION['user_email'] = $row['email'];
-         $_SESSION['user_id'] = $row['id'];
-         header('location:home.php');
-
+      
+      // Decrypt stored password
+      $stored_pass = caesarDecipher($row['password']);
+      // Compare with user input
+      if($filter_pass === $stored_pass){
+         if($row['user_type'] == 'admin'){
+            $_SESSION['admin_name'] = $row['name'];
+            $_SESSION['admin_email'] = $row['email'];
+            $_SESSION['admin_id'] = $row['id'];
+            header('location:admin_page.php');
+         }elseif($row['user_type'] == 'user'){
+            $_SESSION['user_name'] = $row['name'];
+            $_SESSION['user_email'] = $row['email'];
+            $_SESSION['user_id'] = $row['id'];
+            header('location:home.php');
+         }
       }else{
-         $message[] = 'no user found!';
+         $message[] = 'incorrect password!';
       }
-
    }else{
-      $message[] = 'incorrect email or password!';
+      $message[] = 'incorrect email!';
    }
-
 }
 
 ?>
--- register.php
+++ register.php
@@ -49,7 +49,6 @@ if(isset($_POST['submit'])){
          header('location:login.php');
       }
    }
-
 }
 
 ?>
